56925e98e9811eba65e776b136fce23fed7525cd
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-05-24 19:08:47 -0300

    feat: insert recomendados

--- src/pages/ResultSearch/ResultSearch.jsx
+++ src/pages/ResultSearch/ResultSearch.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { GlobalContext } from "../../providers/globalContext";
 import { useContext } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, SimpleGrid } from "@chakra-ui/react";
 import { EventCard } from "../../components/EventCard/EventCard";
 
 export const ResultSearch = () => {
@@ -18,12 +18,40 @@ export const ResultSearch = () => {
       <Text fontWeight="bold" fontSize="3xl" mb="4">
         Resultado da busca
       </Text>
-      <Text> Você buscou por: {searchQuery}</Text>
-      {filteredEvents.map((event) => (
-        <Box key={event.id} padding="10px" margin="10px 0">
-          <EventCard event={event} />
-        </Box>
-      ))}
+      {searchQuery > 0 && filteredEvents.length > 0 ? (
+        <>
+          <Text> Você buscou por: {searchQuery}</Text>
+          {filteredEvents.map((event) => (
+            <Box key={event.id} padding="10px" margin="10px 0">
+              <EventCard event={event} />
+            </Box>
+          ))}
+        </>
+      ) : (
+        <div>
+          <Text>
+            Não foi encontrado nenhum evento com o termo: {searchQuery}
+          </Text>
+          <Text fontWeight="bold" fontSize="3xl" mb="4">
+            Recomendados
+          </Text>
+          {
+            <SimpleGrid
+              columns={[1, 2, 3]}
+              spacing="5"
+              flexWrap="wrap"
+              padding="10px"
+              margin="10px 0"
+            >
+              {events.map((event) => (
+                <Box key={event.id} padding="10px" margin="10px 0">
+                  <EventCard event={event} />
+                </Box>
+              ))}
+            </SimpleGrid>
+          }
+        </div>
+      )}
     </div>
   );
 };
